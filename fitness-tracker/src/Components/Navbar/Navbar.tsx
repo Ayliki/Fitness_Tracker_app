a4f20b1ac0ef1b05e9161254f87e43286fcf0787
Ilya Belov <ilyabelov518@gmail.com>
2024-10-06 23:35:05 +0200

    Fix column alignment in activity log

--- fitness-tracker/src/Components/Navbar/Navbar.tsx
+++ fitness-tracker/src/Components/Navbar/Navbar.tsx
@@ -6,7 +6,7 @@ const Navbar = () => {
     return (
         <nav className={cl.nav}>
             <li><Link to="/">Home</Link></li>
-            <li><Link to="/dashboard">Dashboard</Link></li>
+            <li><Link to="/stats">Dashboard</Link></li>
             <li><Link to="/activity-log">Activity Log</Link></li>
             <li><Link to="/profile">Profile</Link></li>
         </nav>
